930757ebdb4b387fcf4c484775d31f7c6a97f712
adenosinetp10 <adenosinetp10@gmail.com>
2021-02-19 14:42:19 +0530

    added /aa command

--- hisoka.py
+++ hisoka.py
@@ -5,126 +5,152 @@ import random
 from imgProcess import *
 from telegram.ext import Updater, CommandHandler, Dispatcher, CallbackContext
 
-def meme(update:Update,context:CallbackContext)->None:
-    a = meme_generate()
-    update.message.reply_photo(open('m_img.png','rb'),caption=a,quote=False)
 
-def get(update,context):
+def meme(update: Update, context: CallbackContext) -> None:
+    a = meme_generate()
+    update.message.reply_photo(open('m_img.png', 'rb'), caption=a, quote=False)
+
+
+def get(update, context):
     try:
-        pfp = update.message.reply_to_message.from_user.get_profile_photos().photos[0][0].get_file().download()
-        shutil.move(pfp,"pfp/file_1.jpg")
-        pfp = update.message.from_user.get_profile_photos().photos[0][0].get_file().download()
-        shutil.move(pfp,"pfp/file_0.jpg")
+        pfp = update.message.reply_to_message.from_user.get_profile_photos(
+        ).photos[0][0].get_file().download()
+        shutil.move(pfp, "pfp/file_1.jpg")
+        pfp = update.message.from_user.get_profile_photos(
+        ).photos[0][0].get_file().download()
+        shutil.move(pfp, "pfp/file_0.jpg")
     except AttributeError:
         update.message.reply_text("Reply to an User!")
 
-def drake(update:Update,context:CallbackContext)->None:
-    get(update,context)
+
+def drake(update: Update, context: CallbackContext) -> None:
+    get(update, context)
     drake_meme()
     update.message.reply_photo(open('output.jpg', 'rb'))
     os.remove('output.jpg')
 
-def slap(update:Update,context:CallbackContext)->None:
-    get(update,context)
+
+def slap(update: Update, context: CallbackContext) -> None:
+    get(update, context)
     batman_slap()
     update.message.reply_photo(open('output.jpg', 'rb'))
     os.remove('output.jpg')
-    
-def shit(update:Update,context:CallbackContext)->None:
-    get(update,context)
+
+
+def shit(update: Update, context: CallbackContext) -> None:
+    get(update, context)
     ew_stepped_in_shit()
     update.message.reply_photo(open('output.jpg', 'rb'))
     os.remove('output.jpg')
 
-def cat(update:Update,context:CallbackContext)->None:
-    get(update,context)
+
+def cat(update: Update, context: CallbackContext) -> None:
+    get(update, context)
     woman_yelling_at_cat()
     update.message.reply_photo(open('output.jpg', 'rb'))
     os.remove('output.jpg')
 
-def forme(update:Update,context:CallbackContext)->None:
-    get(update,context)
+
+def forme(update: Update, context: CallbackContext) -> None:
+    get(update, context)
     is_for_me()
     update.message.reply_photo(open('output.jpg', 'rb'))
     os.remove('output.jpg')
-    
-def butterfly(update:Update,context:CallbackContext)->None:
-    get(update,context)
+
+
+def butterfly(update: Update, context: CallbackContext) -> None:
+    get(update, context)
     is_that_butterfly()
     update.message.reply_photo(open('output.jpg', 'rb'))
     os.remove('output.jpg')
 
-def bruh(update:Update,context:CallbackContext)->None:
-    get(update,context)
+
+def bruh(update: Update, context: CallbackContext) -> None:
+    get(update, context)
     angry_pakistan_fan()
     update.message.reply_photo(open('output.jpg', 'rb'))
     os.remove('output.jpg')
 
-def strong(update:Update,context:CallbackContext)->None:
-    get(update,context)
+
+def strong(update: Update, context: CallbackContext) -> None:
+    get(update, context)
     strong_doge_weak_doge()
     update.message.reply_photo(open('output.jpg', 'rb'))
     os.remove('output.jpg')
 
-def weak(update:Update,context:CallbackContext)->None:
-    get(update,context)
+
+def weak(update: Update, context: CallbackContext) -> None:
+    get(update, context)
     weak_doge()
     update.message.reply_photo(open('output.jpg', 'rb'))
     os.remove('output.jpg')
 
-def fact(update:Update,context:CallbackContext)->None:
-    get(update,context)
+
+def fact(update: Update, context: CallbackContext) -> None:
+    get(update, context)
     facts_book()
     update.message.reply_photo(open('output.jpg', 'rb'))
     os.remove('output.jpg')
 
-def commands(update:Update,context:CallbackContext)->None:
-    update.message.reply_text("<pre>/meme</pre>\n<pre>/slap</pre>\n<pre>/drake</pre>\n<pre>/cat</pre>\n<pre>/forme</pre>\n<pre>/butterfly</pre>\n<pre>/fact</pre>\n<pre>/weak</pre>\n<pre>/strong</pre>\n<pre>/bruh</pre>\n<pre>/commands</pre>",parse_mode = 'HTML')
 
-def insult(update : Update, context : CallbackContext) -> None:
+def aa(update: Update, context: CallbackContext) -> None:
+    cxt = " ".join(context.args)
+    htv_aliens_guy(cxt)
+    update.message.reply_photo(open('output.jpg', 'rb'), quote=False)
+    os.remove('output.jpg')
+
+
+def commands(update: Update, context: CallbackContext) -> None:
+    update.message.reply_text(
+        "<pre>/meme</pre>\n<pre>/slap</pre>\n<pre>/drake</pre>\n<pre>/cat</pre>\n<pre>/forme</pre>\n<pre>/butterfly</pre>\n<pre>/fact</pre>\n<pre>/weak</pre>\n<pre>/strong</pre>\n<pre>/bruh</pre>\n<pre>/availcommands</pre>\n<pre>/aa {your query} e.g /aa indians</pre>", parse_mode='HTML')
+
+
+def insult(update: Update, context: CallbackContext) -> None:
     print(update.message.from_user.username)
     try:
         if update.message.reply_to_message.from_user.username == 'hisokaDankBot':
-            update.message.reply_text("Did you know?\nBungee Gum possesses the properties of both rubber and gum.\nDon't try anything funny with me bro.")
+            update.message.reply_text(
+                "Did you know?\nBungee Gum possesses the properties of both rubber and gum.\nDon't try anything funny with me bro.")
         else:
             try:
                 username_quote = '@'+update.message.reply_to_message.from_user.username
             except TypeError:
                 username_quote = update.message.reply_to_message.from_user.first_name
             try:
-                username_user = '@'+update.message.from_user.username 
+                username_user = '@'+update.message.from_user.username
             except TypeError:
                 username_user = update.message.from_user.first_name
             with open('insult.txt') as f:
                 insult = random.choice(f.readlines())
                 if "##name##" in insult:
-                    insult = insult.replace("##name##",username_quote)
-                    update.message.reply_text(insult,quote = False)
+                    insult = insult.replace("##name##", username_quote)
+                    update.message.reply_text(insult, quote=False)
                 else:
                     update.message.reply_text(f'{username_user} {insult}\n\nYou just got played yourself.\n'
-                                                'Remember Bungee Gum?\nJust like that, this possesses both\n'
-                                                'the properties of insulting and getting insulted.\nHave a Nice Day :)',quote = False)
-    except AttributeError :
+                                              'Remember Bungee Gum?\nJust like that, this possesses both\n'
+                                              'the properties of insulting and getting insulted.\nHave a Nice Day :)', quote=False)
+    except AttributeError:
         update.message.reply_text('Reply to a User, Idiot!')
 
-if __name__ == "__main__":
-    
-    bot_token = os.environ.get("BOT_TOKEN","")
-    updater = Updater(bot_token,use_context = True)
-    dp = updater.dispatcher
-    dp.add_handler(CommandHandler("drake",drake,run_async = True))
-    dp.add_handler(CommandHandler("slap",slap,run_async = True))
-    dp.add_handler(CommandHandler("shit",shit,run_async = True))
-    dp.add_handler(CommandHandler("cat",cat,run_async = True))
-    dp.add_handler(CommandHandler("forme",forme,run_async = True))
-    dp.add_handler(CommandHandler("meme",meme,run_async = True))
-    dp.add_handler(CommandHandler("butterfly",butterfly,run_async = True))
-    dp.add_handler(CommandHandler("fact",fact,run_async = True))
-    dp.add_handler(CommandHandler("weak",weak,run_async = True))
-    dp.add_handler(CommandHandler("strong",strong,run_async = True))
-    dp.add_handler(CommandHandler("bruh",bruh,run_async = True))
-    dp.add_handler(CommandHandler("availCommands",commands,run_async = True))
-    dp.add_handler(CommandHandler('hinsult',insult,run_async= True))
 
+if __name__ == "__main__":
+
+    bot_token = os.environ.get("BOT_TOKEN", "")
+    updater = Updater(bot_token, use_context=True)
+    dp = updater.dispatcher
+    dp.add_handler(CommandHandler("drake", drake, run_async=True))
+    dp.add_handler(CommandHandler("slap", slap, run_async=True))
+    dp.add_handler(CommandHandler("shit", shit, run_async=True))
+    dp.add_handler(CommandHandler("cat", cat, run_async=True))
+    dp.add_handler(CommandHandler("forme", forme, run_async=True))
+    dp.add_handler(CommandHandler("meme", meme, run_async=True))
+    dp.add_handler(CommandHandler("butterfly", butterfly, run_async=True))
+    dp.add_handler(CommandHandler("fact", fact, run_async=True))
+    dp.add_handler(CommandHandler("weak", weak, run_async=True))
+    dp.add_handler(CommandHandler("strong", strong, run_async=True))
+    dp.add_handler(CommandHandler("bruh", bruh, run_async=True))
+    dp.add_handler(CommandHandler("availCommands", commands, run_async=True))
+    dp.add_handler(CommandHandler('hinsult', insult, run_async=True))
+    dp.add_handler(CommandHandler('aa', aa, run_async=True))
     updater.start_polling()
     updater.idle()
--- imgProcess.py
+++ imgProcess.py
@@ -1,26 +1,30 @@
 from PIL import Image, ImageDraw, ImageFont
-import os, requests, json
+import os
+import requests
+import json
 
 MEME_PATH = 'templates/'
 FONT_PATH = 'fonts/'
 PFP_PATH = 'pfp/'
-R_MEME_URL = os.environ.get("meme_api","")
+R_MEME_URL = os.environ.get("meme_api", "")
 
-fnt = ImageFont.truetype(FONT_PATH+'Menlo-Regular.ttf',size= 20)
+fnt = ImageFont.truetype(FONT_PATH+'Menlo-Regular.ttf', size=20)
+
 
 def meme_generate():
     r = requests.get(R_MEME_URL).json()
-    with open('m_img.png','wb') as f:
+    with open('m_img.png', 'wb') as f:
         f.write(requests.get(r['url']).content)
     return r['title']
+
 
 def drake_meme():
     meme = Image.open(MEME_PATH+'drake.jpg')
     img1 = Image.open(PFP_PATH+'file_0.jpg')
     img2 = Image.open(PFP_PATH+'file_1.jpg')
 
-    meme.paste(img2,(210,10))
-    meme.paste(img1,(210,210))
+    meme.paste(img2, (210, 10))
+    meme.paste(img1, (210, 210))
 
     #draw = ImageDraw.Draw(meme)
     #draw.text((210,0),text=QUOTENAME,fill=(3, 132, 252),font=fnt)
@@ -30,13 +34,14 @@ def drake_meme():
     os.remove(PFP_PATH+'file_0.jpg')
     os.remove(PFP_PATH+'file_1.jpg')
 
+
 def batman_slap():
     meme = Image.open(MEME_PATH+'batman_slap.jpg')
-    img1 = Image.open(PFP_PATH+'file_0.jpg').resize((100,100))
-    img2 = Image.open(PFP_PATH+'file_1.jpg').resize((100,100))
+    img1 = Image.open(PFP_PATH+'file_0.jpg').resize((100, 100))
+    img2 = Image.open(PFP_PATH+'file_1.jpg').resize((100, 100))
 
-    meme.paste(img2,(110,200))
-    meme.paste(img1,(248,134))
+    meme.paste(img2, (110, 200))
+    meme.paste(img1, (248, 134))
 
     #draw = ImageDraw.Draw(meme)
     #draw.text((110,200),text=str(QUOTENAME),fill=(98, 3, 252),font=fnt)
@@ -46,13 +51,14 @@ def batman_slap():
     os.remove(PFP_PATH+'file_0.jpg')
     os.remove(PFP_PATH+'file_1.jpg')
 
+
 def ew_stepped_in_shit():
     meme = Image.open(MEME_PATH+'ew_stepped_in_shit.jpg')
-    img1 = Image.open(PFP_PATH+'file_0.jpg').resize((51,44))
-    img2 = Image.open(PFP_PATH+'file_1.jpg').resize((92,98))
+    img1 = Image.open(PFP_PATH+'file_0.jpg').resize((51, 44))
+    img2 = Image.open(PFP_PATH+'file_1.jpg').resize((92, 98))
 
-    meme.paste(img2,(65,189))
-    meme.paste(img1,(109,52))
+    meme.paste(img2, (65, 189))
+    meme.paste(img1, (109, 52))
 
     #draw = ImageDraw.Draw(meme)
     #draw.text((210,0),text=QUOTENAME,fill=(3, 132, 252),font=fnt)
@@ -61,14 +67,15 @@ def ew_stepped_in_shit():
     meme.save('output.jpg')
     os.remove(PFP_PATH+'file_0.jpg')
     os.remove(PFP_PATH+'file_1.jpg')
+
 
 def is_that_butterfly():
     meme = Image.open(MEME_PATH+'is_that_butterfly.jpg')
-    img1 = Image.open(PFP_PATH+'file_0.jpg').resize((170,179))
-    img2 = Image.open(PFP_PATH+'file_1.jpg').resize((103,93))
+    img1 = Image.open(PFP_PATH+'file_0.jpg').resize((170, 179))
+    img2 = Image.open(PFP_PATH+'file_1.jpg').resize((103, 93))
 
-    meme.paste(img2,(401,0))
-    meme.paste(img1,(104,41))
+    meme.paste(img2, (401, 0))
+    meme.paste(img1, (104, 41))
 
     #draw = ImageDraw.Draw(meme)
     #draw.text((210,0),text=QUOTENAME,fill=(3, 132, 252),font=fnt)
@@ -77,14 +84,15 @@ def is_that_butterfly():
     meme.save('output.jpg')
     os.remove(PFP_PATH+'file_0.jpg')
     os.remove(PFP_PATH+'file_1.jpg')
+
 
 def woman_yelling_at_cat():
     meme = Image.open(MEME_PATH+'woman_yelling_at_cat.jpg')
-    img1 = Image.open(PFP_PATH+'file_0.jpg').resize((115,96))
-    img2 = Image.open(PFP_PATH+'file_1.jpg').resize((122,164))
+    img1 = Image.open(PFP_PATH+'file_0.jpg').resize((115, 96))
+    img2 = Image.open(PFP_PATH+'file_1.jpg').resize((122, 164))
 
-    meme.paste(img2,(0,34))
-    meme.paste(img1,(364,75))
+    meme.paste(img2, (0, 34))
+    meme.paste(img1, (364, 75))
 
     #draw = ImageDraw.Draw(meme)
     #draw.text((210,0),text=QUOTENAME,fill=(3, 132, 252),font=fnt)
@@ -93,13 +101,13 @@ def woman_yelling_at_cat():
     meme.save('output.jpg')
     os.remove(PFP_PATH+'file_0.jpg')
     os.remove(PFP_PATH+'file_1.jpg')
+
 
 def is_for_me():
     meme = Image.open(MEME_PATH+'is_for_me.jpg')
-    img1 = Image.open(PFP_PATH+'file_0.jpg').resize((177,161))
-    
+    img1 = Image.open(PFP_PATH+'file_0.jpg').resize((177, 161))
 
-    meme.paste(img1,(129,79))
+    meme.paste(img1, (129, 79))
 
     #draw = ImageDraw.Draw(meme)
     #draw.text((210,0),text=QUOTENAME,fill=(3, 132, 252),font=fnt)
@@ -108,14 +116,15 @@ def is_for_me():
     meme.save('output.jpg')
     os.remove(PFP_PATH+'file_0.jpg')
     os.remove(PFP_PATH+'file_1.jpg')
+
 
 def weak_doge():
     meme = Image.open(MEME_PATH+'weak_doge.jpg')
-    img1 = Image.open(PFP_PATH+'file_0.jpg').resize((76,66))
+    img1 = Image.open(PFP_PATH+'file_0.jpg').resize((76, 66))
     #img2 = Image.open(PFP_PATH+'file_1.jpg').resize((92,98))
 
-    #meme.paste(img2,(65,189))
-    meme.paste(img1,(60,33))
+    # meme.paste(img2,(65,189))
+    meme.paste(img1, (60, 33))
 
     #draw = ImageDraw.Draw(meme)
     #draw.text((210,0),text=QUOTENAME,fill=(3, 132, 252),font=fnt)
@@ -124,14 +133,15 @@ def weak_doge():
     meme.save('output.jpg')
     os.remove(PFP_PATH+'file_0.jpg')
     os.remove(PFP_PATH+'file_1.jpg')
+
 
 def strong_doge_weak_doge():
     meme = Image.open(MEME_PATH+'strong_doge_weak_doge.jpg')
-    img1 = Image.open(PFP_PATH+'file_0.jpg').resize((46,44))
-    img2 = Image.open(PFP_PATH+'file_1.jpg').resize((48,41))
+    img1 = Image.open(PFP_PATH+'file_0.jpg').resize((46, 44))
+    img2 = Image.open(PFP_PATH+'file_1.jpg').resize((48, 41))
 
-    meme.paste(img2,(295,172))
-    meme.paste(img1,(110,79))
+    meme.paste(img2, (295, 172))
+    meme.paste(img1, (110, 79))
 
     #draw = ImageDraw.Draw(meme)
     #draw.text((210,0),text=QUOTENAME,fill=(3, 132, 252),font=fnt)
@@ -140,14 +150,15 @@ def strong_doge_weak_doge():
     meme.save('output.jpg')
     os.remove(PFP_PATH+'file_0.jpg')
     os.remove(PFP_PATH+'file_1.jpg')
+
 
 def angry_pakistan_fan():
     meme = Image.open(MEME_PATH+'angry_pakistan_fan.jpg')
-    img1 = Image.open(PFP_PATH+'file_0.jpg').resize((68,85))
+    img1 = Image.open(PFP_PATH+'file_0.jpg').resize((68, 85))
     #img2 = Image.open(PFP_PATH+'file_1.jpg').resize((48,41))
 
-    #meme.paste(img2,(295,172))
-    meme.paste(img1,(170,16))
+    # meme.paste(img2,(295,172))
+    meme.paste(img1, (170, 16))
 
     #draw = ImageDraw.Draw(meme)
     #draw.text((210,0),text=QUOTENAME,fill=(3, 132, 252),font=fnt)
@@ -156,13 +167,14 @@ def angry_pakistan_fan():
     meme.save('output.jpg')
     os.remove(PFP_PATH+'file_0.jpg')
     os.remove(PFP_PATH+'file_1.jpg')
+
 
 def facts_book():
     meme = Image.open(MEME_PATH+'facts_book.jpg')
-    img1 = Image.open(PFP_PATH+'file_0.jpg').resize((49,48))
-    img2 = Image.open(PFP_PATH+'file_1.jpg').resize((49,48))
-    meme.paste(img2,(17,391))
-    meme.paste(img1,(34,319))
+    img1 = Image.open(PFP_PATH+'file_0.jpg').resize((49, 48))
+    img2 = Image.open(PFP_PATH+'file_1.jpg').resize((49, 48))
+    meme.paste(img2, (17, 391))
+    meme.paste(img1, (34, 319))
 
     #draw = ImageDraw.Draw(meme)
     #draw.text((210,0),text=QUOTENAME,fill=(3, 132, 252),font=fnt)
@@ -171,3 +183,16 @@ def facts_book():
     meme.save('output.jpg')
     os.remove(PFP_PATH+'file_0.jpg')
     os.remove(PFP_PATH+'file_1.jpg')
+
+
+def htv_aliens_guy(name):
+    message = name.replace(' ', '\n')
+    img = Image.open(MEME_PATH+'aa.jpg')
+    draw = ImageDraw.Draw(img)
+    bounding_box = [64, 183, 170, 215]
+    x1, y1, x2, y2 = bounding_box
+    w, h = draw.textsize(message, font=fnt)
+    x = (x2 - x1 - w)/2 + x1
+    y = (y2 - y1 - h)/2 + y1
+    draw.text((x, y), message, align='center', font=fnt)
+    img.save('output.png')
