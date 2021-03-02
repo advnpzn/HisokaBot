62420abc9ff2151e22a9636b4bb341ae6274f626
adenosinetp10 <63637385+adenosinetp10@users.noreply.github.com>
2021-03-02 11:10:14 +0530

    integrated manga with anime

--- HisokaBot/funcs/anime.py
+++ HisokaBot/funcs/anime.py
@@ -5,23 +5,27 @@ from telegram.ext import CallbackQueryHandler, CallbackContext
 from HisokaBot import dp
 
 user_anime_dict = {}
+user_manga_dict = {}
 
 
-def search_anime(name):
+def search_anime_manga(name, stype):
     name = {
         'search': name,
         'page': 1,
         'perPage': 5,
-        'type': 'ANIME'
+        'type': stype
     }
     r = requests.post(ANILIST_GRAPHQL_URI, json={'query': searchAnime, 'variables': name}).json()
     return r['data']['Page']['media']
 
 
-def anime(update: Update, context: CallbackContext, name):
-    data = search_anime(name)
+def anime_manga(update: Update, context: CallbackContext, name, stype):
+    data = search_anime_manga(name, stype)
     user = update.effective_user
-    user_anime_dict[user.id] = data
+    if stype == 'ANIME':
+        user_anime_dict[user.id] = data
+    elif stype == 'MANGA':
+        user_manga_dict[user.id] = data
     no_of_buttons = len(data)
     btn_list = []
     for i in range(no_of_buttons):
@@ -29,23 +33,28 @@ def anime(update: Update, context: CallbackContext, name):
             [
                 InlineKeyboardButton(
                     text=data[i]['title']['english'] if data[i]['title']['english'] else data[i]['title']['romaji'],
-                    callback_data=f"animesearch_{i}_{user.id}")
+                    callback_data=f"animesearch_{i}_{user.id}_{stype}")
             ]
         )
     update.message.reply_text(text="Your Search Result(s) :", reply_markup=InlineKeyboardMarkup(btn_list))
 
 
-def anime_when_clicked(update: Update, context: CallbackContext):
+def anime_manga_when_clicked(update: Update, context: CallbackContext):
     query = update.callback_query
-    match = query.data.split('_')  # match[0] = anime, match[1] = the anime's serial no., match[2] = user_id
+    match = query.data.split('_')  # match[0] = anime, match[1] = s_no., match[2] = user_id
     s_no = int(match[1])
+    user_id = int(match[2])
+    s_type = match[3]
     user = update.effective_user
-    if user.id != int(match[2]):
+    if user.id != user_id:
         query.answer(text="Not allowed!", show_alert=True)
         return -1
     query.answer("Wait..")
     query.message.delete()
-    data = user_anime_dict[int(match[2])][s_no]
+    if s_type == 'ANIME':
+        data = user_anime_dict[user_id][s_no]
+    elif s_type == 'MANGA':
+        data = user_manga_dict[user_id][s_no]
     caption = ANIME_STR.format(
         data['title']['english'],
         data['title']['romaji'],
@@ -70,7 +79,6 @@ def anime_when_clicked(update: Update, context: CallbackContext):
         replace(f"\(https://img\.anili\.st/media/{data['id']}\)",
                 f"(https://img\.anili\.st/media/{data['id']})"). \
         replace("None", "").replace("-", "\-")
-    #    print(caption)
     context.bot.send_message(chat_id=update.effective_chat.id,
                              text=caption,
                              parse_mode=ParseMode.MARKDOWN_V2,
@@ -81,4 +89,4 @@ def anime_when_clicked(update: Update, context: CallbackContext):
                              ))
 
 
-dp.add_handler(CallbackQueryHandler(anime_when_clicked, pattern=r"animesearch_"))
+dp.add_handler(CallbackQueryHandler(anime_manga_when_clicked, pattern=r"animesearch_"))
