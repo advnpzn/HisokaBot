e59ce78f028b294ea63587a0f494b015ca879f6c
adenosinetp10 <63637385+adenosinetp10@users.noreply.github.com>
2021-03-02 11:31:39 +0530

    Update __main__.py

--- HisokaBot/__main__.py
+++ HisokaBot/__main__.py
@@ -151,12 +151,12 @@ def anime_cmd(update: Update, context: CallbackContext):
                                   reply_markup=ForceReply(force_reply=True))
         return 69
     else:
-        anime(update, context, query)
+        anime_manga(update, context, query)
 
 
 def anime_state(update: Update, context: CallbackContext):
     query = update.message.text
-    anime(update, context, query)
+    anime_manga(update, context, query)
     return 420
 
 
