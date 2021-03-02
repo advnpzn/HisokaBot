cd00bbddad40a1585425108a4b53cf2352f4b8e5
adenosinetp10 <63637385+adenosinetp10@users.noreply.github.com>
2021-03-02 20:03:27 +0530

    Update __init__.py

--- HisokaBot/__init__.py
+++ HisokaBot/__init__.py
@@ -1,5 +1,15 @@
 from telegram.ext import Updater
+import logging
 from HisokaBot.helpers.constants import BOT_TOKEN
+
+
+# Enable logging
+logging.basicConfig(
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO
+)
+
+logger = logging.getLogger(__name__)
+logger.info(msg="Logging started...")
 
 updater = Updater(BOT_TOKEN)
 dp = updater.dispatcher
