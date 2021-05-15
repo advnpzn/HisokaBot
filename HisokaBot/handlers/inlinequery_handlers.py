95b508996ed2493c201630401e71b574eb6fcbdc
adenosinetp10 <63637385+adenosinetp10@users.noreply.github.com>
2021-05-16 00:11:15 +0530

    Update inlinequery_handlers.py

--- HisokaBot/handlers/inlinequery_handlers.py
+++ HisokaBot/handlers/inlinequery_handlers.py
@@ -1,6 +1,7 @@
 from HisokaBot.helpers.constants import searchAnime
+import nekos
 from telegram import Update, InlineQueryResultArticle, InputTextMessageContent, InlineKeyboardMarkup, \
-    InlineKeyboardButton, ParseMode
+    InlineKeyboardButton, ParseMode, InlineQueryResultPhoto
 from telegram.ext import InlineQueryHandler, CallbackContext
 from HisokaBot.helpers.anime_manga_htm_2_mdv2 import anime_manga_html_2_mdv2
 from telegram.constants import MAX_MESSAGE_LENGTH
@@ -11,13 +12,21 @@ from HisokaBot.helpers.constants import ANIME_STR, searchChars
 
 def inline_search(update: Update, context: CallbackContext):
     query = update.inline_query.query
-    to_search = ['Anime', 'Manga', 'Character']
+        to_search = ['Anime', 'Manga', 'Character', 'Owo', 'Neko', 'Cheemsify', 'tc']
     if to_search[2] in query:
         inline_search_character(update, context)
     elif to_search[1] in query:
         inline_search_anime_manga(update, context, to_search[1])
     elif to_search[0] in query:
         inline_search_anime_manga(update, context, to_search[0])
+    elif to_search[3] in query:
+        inline_search_owoify(update, context)
+    elif to_search[4] in query:
+        inline_search_neko(update, context)
+    elif to_search[5] in query:
+        inline_search_cheemsify(update, context)
+    elif to_search[6] in query:
+        inline_search_tc(update, context)
     else:
         res = []
         for i in range(0, len(to_search)):
@@ -182,3 +191,72 @@ def inline_search_anime_manga(update: Update, context: CallbackContext, stype: s
                 )
             )
         update.inline_query.answer(results=res)
+        
+        
+ 
+def inline_search_tc(update: Update, context: CallbackContext):
+    query = update.inline_query.query
+    s = query.replace('tc', '')
+    update.inline_query.answer(results=[
+    InlineQueryResultArticle(
+                 id=uuid4(),
+                 title='Textcat',
+                 input_message_content=InputTextMessageContent(message_text=nekos.textcat())
+                 )])
+
+
+def inline_search_neko(update: Update, context: CallbackContext):
+    query = update.inline_query.query
+    s = query.replace('tc', '')
+    update.inline_query.answer(results=[
+    InlineQueryResultPhoto(
+        id=uuid4(),
+        photo_url=nekos.cat(),
+        title='Neko'
+    )])
+
+def inline_search_owoify(update: Update, context: CallbackContext):
+    query = update.inline_query.query
+    s = query.replace('Owo', '')
+    update.inline_query.answer(results=[
+    InlineQueryResultArticle(
+                 id=uuid4(),
+                 title='Owo',
+                 input_message_content=InputTextMessageContent(message_text=nekos.owoify(s))
+                 )])
+
+
+def inline_search_cheemsify(update: Update, context: CallbackContext):
+    query = update.inline_query.query
+    text = query.replace('Cheemsify', '').split(" ")
+    holyWords = {
+    'burger': 'burmger',
+    'bad': 'bamd',
+    'batman': 'bamtman',
+    'cheese': 'cheems',
+    'cheems': 'cheems',
+    'cheeseburger': 'cheemsburger',
+    'doge': 'domge',
+    'female': 'f*male',
+    'history': 'himstory',
+    'nigger': 'n-word',
+    'nigga': 'n-word',
+    'retard': 'remtard',
+    'woman': 'w*man',
+    'women': 'w*men',
+    'walter': 'walmter',
+    'motherfucker': 'momtherfumcker',
+    }
+
+    for i in range(len(text)):
+        if text[i] in holyWords.keys():
+            text[i] = holyWords[text[i]]
+
+    test = " ".join(text)
+    cheemsed = test.replace('ck', 'mck').replace('ll', 'mll').replace('n', 'mn')
+    update.inline_query.answer(results=[
+    InlineQueryResultArticle(
+                 id=uuid4(),
+                 title='Cheemsify',
+                 input_message_content=InputTextMessageContent(message_text=cheemsed)
+                 )])
